26e9a181dc4b5044d9958ba5b4b0fc47021211bc
Lassi Konttinen <lassikonttinen@gmail.com>
2024-08-19 13:21:23 +0300

    notes about getaddrinfo

--- Server.hpp
+++ Server.hpp
@@ -11,3 +11,23 @@ public:
   ~Server(void);
 };
 
+/*
+int getaddrinfo(const char *node,             // host name or IP to connect to
+                const char *service,          // "http" or port number
+                const struct addrinfo *hints, // addrinfo* that you’ve already filled out with relevant information.
+                struct addrinfo **res);       // points to a linked list of addrinfos
+*/
+
+/*
+struct addrinfo {
+  int     ai_flags;          // AI_PASSIVE, AI_CANONNAME, ...
+  int     ai_family;         // AF_xxx
+  int     ai_socktype;       // SOCK_xxx
+  int     ai_protocol;       // 0 (auto) or IPPROTO_TCP, IPPROTO_UDP 
+
+  socklen_t  ai_addrlen;     // length of ai_addr
+  char   *ai_canonname;      // canonical name for nodename
+  struct sockaddr  *ai_addr; // binary address
+  struct addrinfo  *ai_next; // next structure in linked list
+};
+*/
