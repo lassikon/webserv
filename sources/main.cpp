0013783688c0714981e2051c0748010ea4d05d64
Jarnomer <jankkumer@pm.me>
2024-08-22 20:34:14 +0300

    Added level color and name as template arguments. Reformatted code

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,8 +4,8 @@ int main() {
   LOG_TRACE("Hello ", 123);
   LOG_DEBUG("Hello ", 123);
   LOG_INFO("Hello ", 123);
-  LOG_WARNING("Hello ", 123);
+  LOG_WARN("Hello ", 123);
   LOG_ERROR("Hello ", 123);
-  LOG_CRITICAL("Hello ", 123);
+  LOG_FATAL("Hello ", 123);
   return 0;
 }
--- sources/utils/Logger.cpp
+++ sources/utils/Logger.cpp
@@ -3,7 +3,7 @@
 Logger::Logger(void) {
   currentLevel = logLevel::Trace;
   currentOutput = logOutput::Both;
-  enabledDetail[(int)logDetail::TimeStamp] = true;
+  enabledDetail[(int)logDetail::TimeStamp] = false;
   enabledDetail[(int)logDetail::SourceFile] = true;
   enabledDetail[(int)logDetail::LineNumber] = true;
   if (currentOutput != logOutput::ConsoleOnly)
@@ -13,51 +13,14 @@ Logger::Logger(void) {
 Logger::~Logger(void) { closeLogFile(); }
 
 void Logger::createLogFile(void) {
-  logFile.open(logFileName, std::fstream::out);
+  logFile.open(fileName, std::fstream::out);
   if (logFile.fail())
-    std::cerr << logFileName << ": " << strerror(errno) << std::endl;
+    std::cerr << fileName << ": " << strerror(errno) << std::endl;
 }
 
 void Logger::closeLogFile(void) {
   if (logFile.is_open())
     logFile.close();
-}
-
-void Logger::setLogLevel(logLevel newLevel) { currentLevel = newLevel; }
-void Logger::setLogOutput(logOutput newOutput) { currentOutput = newOutput; }
-
-std::vector<std::string> Logger::getLogInfo(logLevel Level) {
-  std::vector<std::string> logInfo;
-  switch (Level) {
-  case logLevel::Trace:
-    logInfo.push_back("TRACE");
-    logInfo.push_back(CYAN);
-    return logInfo;
-  case logLevel::Debug:
-    logInfo.push_back("DEBUG");
-    logInfo.push_back(GREEN);
-    return logInfo;
-  case logLevel::Info:
-    logInfo.push_back("INFO");
-    logInfo.push_back(BLUE);
-    return logInfo;
-  case logLevel::Warn:
-    logInfo.push_back("WARNING");
-    logInfo.push_back(YELLOW);
-    return logInfo;
-  case logLevel::Error:
-    logInfo.push_back("ERROR");
-    logInfo.push_back(RED);
-    return logInfo;
-  case logLevel::Critical:
-    logInfo.push_back("CRITICAL");
-    logInfo.push_back(RED);
-    return logInfo;
-  default:
-    logInfo.push_back("UNKNOWN");
-    logInfo.push_back(WHITE);
-    return logInfo;
-  }
 }
 
 std::string Logger::getTimeStamp(void) const {
