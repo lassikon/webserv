77066d165273c03a9373f75375417de805bfa53c
Lassi Konttinen <lassikonttinen@gmail.com>
2024-09-16 14:39:31 +0300

    merge with janrau

--- sources/request/handlers/DeleteHandler.cpp
+++ sources/request/handlers/DeleteHandler.cpp
@@ -1,7 +1,9 @@
-#include <DeleteHandler.hpp>
 #include <Client.hpp>
+#include <DeleteHandler.hpp>
 
 void DeleteHandler::executeRequest(Client& client) {
   LOG_INFO("Processing DELETE request for path:", client.getReq().getReqURI());
   LOG_TRACE("DeleteHandler: executingRequest");
 }
+
+//std::filesystem::remove(filePath)
