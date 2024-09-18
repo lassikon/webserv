2ef1c97c54969f6f238fe5bce16a672c712a2f7a
jankku <jankkume@pm.me>
2024-09-18 14:03:24 +0300

    Reformat in uploader

--- sources/request/handlers/UploadHandler.cpp
+++ sources/request/handlers/UploadHandler.cpp
@@ -9,10 +9,10 @@ void UploadHandler::executeRequest(Client& client) {
   if (client.getRes().getResStatusCode() != 0) {
     return;
   }
-  LOG_INFO("Processing UPLOAD POST request for path:", client.getReq().getReqURI());
-  LOG_TRACE("GetHandler: executingRequest");
+  LOG_INFO("Processing UPLOAD request for path:", client.getReq().getReqURI());
 }
 
+// LOG_TRACE("GetHandler: executingRequest");
 // std::shared_ptr<ProcessTreeBuilder> ptb =
 //   std::make_shared<ProcessTreeBuilder>(client, client.getRes().getServerConfig());
 // //client.getRes().setReqURI(client.getReq().getReqURI());
